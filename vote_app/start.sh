9eac8834788face96948a38a1eabb0946fe289a5
Dejun Xiang <derek@Dejuns-MBP.gateway>
2022-12-02 08:58:07 +1100

    Add argocd config

---
+++ vote_app/start.sh
@@ -0,0 +1,17 @@
+!#/bin/bash
+
+kubectl create -f voting-app-deploy.yaml
+kubectl create -f voting-app-service.yaml
+
+kubectl create -f redis-deploy.yaml
+kubectl create -f redis-service.yaml
+
+kubectl create -f postgres-deploy.yaml
+kubectl create -f postgres-service.yaml
+
+kubectl create -f worker-app-depoy.yaml
+kubectl create -f worker-app-service.yaml
+
+kubectl create -f result-app-deploy.yaml
+kubectl create -f result-app-service.yaml
+
